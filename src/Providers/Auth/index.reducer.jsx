0a01a5a53e4efe2c4591c4648146df6e1c22e540
satrioadii <satriooadi@gmail.com>
2020-08-28 18:56:31 +0700

    TOKEN debug

--- src/Providers/Auth/index.reducer.jsx
+++ src/Providers/Auth/index.reducer.jsx
@@ -26,7 +26,6 @@ const AuthReducer = (state, action) => {
 		case LOGIN_ERROR:
 		case CHECK_AUTH_ERROR:
 		case LOGOUT_REQUEST:
-			localStorage.removeItem("satrio_admin_token");
 			return { ...state, isLoading: false, token: null };
 		default:
 			return state;
